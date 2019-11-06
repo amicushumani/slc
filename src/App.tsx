d815c8d6ae85e4244afa463324d5a8248b5ea7b9
jamesjclstemp <56581238+jamesjclstemp@users.noreply.github.com>
2019-11-07 00:35:36 +0100

    REMOVED router from homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,11 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import './App.css';
 //ts-ignore
 import ProductsComponent from './Pages/Products/index';
 import Navbar from './Components/Navbar';
 
 const App: React.FC = () => {
-  return (
-    <Router>
-      <Navbar></Navbar>
-    </Router>
-  );
+  return <Navbar></Navbar>;
 };
 
 const Home = () => {
